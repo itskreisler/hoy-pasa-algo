c292829cba1ec8f9067dd99e24f1002ccfafa145
Kreisler Ramirez <itskreisler@outlook.com>
2025-07-16 16:21:31 -0500

    feat: Mejorar la configuración del backend y agregar manejo de archivos estáticos

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, send_from_directory
+import os
 from dataclasses import dataclass
+
+dist_sin_resolver = os.path.join(os.path.dirname(__file__), "../dist")
+# hacer un resolve
+dist = os.path.abspath(dist_sin_resolver)
+print(f"Directorio de distribución: {dist}")
+if os.path.exists(dist):
+    # listar de archivos en el directorio
+    files = os.listdir(dist)
+    print(f"Archivos en '{dist}': {files}")
+
+if not os.path.exists(dist):
+    raise FileNotFoundError(f"Directory '{dist}' does not exist. Please build the frontend first.")
 
 
 @dataclass
@@ -10,13 +23,21 @@ class Usuario:
 
 usuarios: list[Usuario] = []
 #
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../dist", static_url_path="")
 
 
 @app.route("/")
 def index():
-    return {"mensaje": "¡Hola, mundo!"}
-    return render_template("index.html", mensaje="Este es un mensaje de prueba")
+    if app.static_folder is None:
+        raise ValueError("Static folder is not configured")
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/<path:path>")
+def static_proxy(path: str):
+    if app.static_folder is None:
+        raise ValueError("Static folder is not configured")
+    return send_from_directory(app.static_folder, path)
 
 
 #
