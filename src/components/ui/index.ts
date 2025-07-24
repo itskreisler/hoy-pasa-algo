7e2a440f9a1a01266e9af2e54b0075974f992592
Kreisler Ramirez <itskreisler@outlook.com>
2025-07-24 13:36:13 -0500

    fix

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,3 +4,9 @@ export { Skeleton } from './Skeleton'
 // Skeleton específicos por tipo de interfaz
 export { ProfileSkeleton } from './ProfileSkeleton'
 export { MobileMenuSkeleton, NavSkeleton } from './NavigationSkeleton'
+
+// Modal reutilizable
+export { Modal } from './Modal'
+
+// Componentes de evento
+export { EventDetails } from './EventDetails'
